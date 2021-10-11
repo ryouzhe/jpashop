8a32591404a3156d132cfee632d0ba81c904da9c
ryouzhe <sjh3183@naver.com>
2021-10-11 21:50:42 +0900

    JPA Shop 회원등록/목록, 상품등록/목록, 상품주문/내역 > 화면 개발

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class ItmeService {
+public class ItemService {
 
     private final ItemRepository itemRepository;
 
